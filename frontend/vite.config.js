e7a08ee4a76576c5e1ddbc1287483b49aa2aa127
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-06-14 15:26:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
-/// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set to root
+  base: '/', // Important for Netlify
+  build: {
+    outDir: 'dist' // This is default
+  }
 })
