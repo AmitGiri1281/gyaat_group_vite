c1290a1090618b0b413782ee45c5b395302a95d6
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-06-14 14:44:54 +0530

    Update vite.config.js making previous

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Use relative path instead
+  base: '/gyaatgroup/', // 👈 Replace with your repo name
 })
+
+
